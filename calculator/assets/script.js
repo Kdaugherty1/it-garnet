22daede2e849dc24f8c182dcd72e401a5a9398df
Kdaugherty1 <145708928+Kdaugherty1@users.noreply.github.com>
2024-11-21 06:35:56 +0000

    all

--- calculator/assets/script.js
+++ calculator/assets/script.js
@@ -1,65 +1,70 @@
 function calculate() {
+  /* Make sure that the form is valid */
+  if ($("#calculator").valid()) {
+    /* get the operands from the form */
+    var operand1 = document.getElementById("Operand1").value;
+    var operand2 = document.getElementById("Operand2").value;
 
-    if ($("#calculator").valid()) {
-     
-      var operand1 = document.getElementById("Operand1").value;
-      var operand2 = document.getElementById("Operand2").value;
-  
-    
-      var operand1fp = parseFloat(operand1);
-      var operand2fp = parseFloat(operand2);
-  
-      
-      var operator;
-  
-      if (document.getElementById("AddOperator").checked) {
-        operator = document.getElementById("AddOperator").value;
-      }
-      if (document.getElementById("SubtractOperator").checked) {
-        operator = document.getElementById("SubtractOperator").value;
-      }
-      if (document.getElementById("MultiplyOperator").checked) {
-        operator = document.getElementById("MultiplyOperator").value;
-      }
-      if (document.getElementById("DivideOperator").checked) {
-        operator = document.getElementById("DivideOperator").value;
-      }
-  
-      var result;
-  
-      if (operator == "Add") {
-        result = operand1fp + operand2fp;
-      }
-  
-      
-      if (operator == "Subtract") {
-        result = operand1fp - operand2fp;
-      }
-  
-  
-      if (operator == "Multiply") {
-        result = operand1fp * operand2fp;
-      }
+    /* convert the operands from string to floating point */
+    var operand1fp = parseFloat(operand1);
+    var operand2fp = parseFloat(operand2);
 
-      if (operator == "Divide") {
-        result = operand1fp / operand2fp;
-      }
-  
-      document.getElementById("Result").innerHTML = result.toString();
+    /* figure out which operator was checked and place the value in operator */
+    var operator;
+
+    if (document.getElementById("AddOperator").checked) {
+      operator = document.getElementById("AddOperator").value;
     }
+    if (document.getElementById("SubtractOperator").checked) {
+      operator = document.getElementById("SubtractOperator").value;
+    }
+    if (document.getElementById("MultiplyOperator").checked) {
+      operator = document.getElementById("MultiplyOperator").value;
+    }
+    if (document.getElementById("DivideOperator").checked) {
+      operator = document.getElementById("DivideOperator").value;
+    }
+
+    var result;
+
+    /* if the operator was "Add" then set result to addition */
+    if (operator == "Add") {
+      result = operand1fp + operand2fp;
+    }
+
+    /* if the operator was "Subtract" then set result to subtraction */
+    if (operator == "Subtract") {
+      result = operand1fp - operand2fp;
+    }
+
+    /* if the operator was "Multiply" then set result to multiplication */
+    if (operator == "Multiply") {
+      result = operand1fp * operand2fp;
+    }
+
+    /* if operator was "Divide" then set result to division */
+    if (operator == "Divide") {
+      result = operand1fp / operand2fp;
+    }
+
+    /* convert the result to a string and display it */
+    document.getElementById("Result").innerHTML = result.toString();
   }
-  
-  function clearform() {
-    document.getElementById("Operand1").value = "";
-    document.getElementById("Operand2").value = "";
-    document.getElementById("Operand1Error").innerHTML = "";
-    document.getElementById("Operand2Error").innerHTML = "";
-    document.getElementById("AddOperator").checked = false;
-    document.getElementById("SubtractOperator").checked = false;
-    document.getElementById("MultiplyOperator").checked = false;
-    document.getElementById("DivideOperator").checked = false;
-    document.getElementById("OperatorError").innerHTML = "";
-    document.getElementById("Result").innerHTML = "";
-  }
-  
-  $("#calculator").validate({});
+}
+
+function clearform() {
+  /* Set all of the form values to blank or false */
+  document.getElementById("Operand1").value = "";
+  document.getElementById("Operand2").value = "";
+  document.getElementById("Operand1Error").innerHTML = "";
+  document.getElementById("Operand2Error").innerHTML = "";
+  document.getElementById("AddOperator").checked = false;
+  document.getElementById("SubtractOperator").checked = false;
+  document.getElementById("MultiplyOperator").checked = false;
+  document.getElementById("DivideOperator").checked = false;
+  document.getElementById("OperatorError").innerHTML = "";
+  document.getElementById("Result").innerHTML = "";
+}
+
+/* Form Validation */
+$("#calculator").validate({});
